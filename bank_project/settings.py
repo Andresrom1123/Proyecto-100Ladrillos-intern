1bd9bf3d70a56c7a6ea95b592c812d4692ed8e41
Andresrom1123 <andres2016_romero@hotmail.com>
2023-03-24 13:54:01 -0600

    add docker compose and change the bd for postgress

--- bank_project/settings.py
+++ bank_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'bank_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bank_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.01',
+        'PORT': '5432',
     }
 }
 
